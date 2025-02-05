f2b5f0058c1373b293447378493dda96920df81e
gaiii123 <gayanmaduwantha789@gmail.com>
2025-02-05 23:30:02 +0530

    procfile added

--- app.py
+++ app.py
@@ -72,6 +72,6 @@ def get_reviews():
     return jsonify(reviews)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
 
 
