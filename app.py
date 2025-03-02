6bceb47c886129fa480242020b40f93df52dc310
gaiii123 <gayanmaduwantha789@gmail.com>
2025-03-02 14:14:34 +0530

    chnged something

--- app.py
+++ app.py
@@ -190,3 +190,5 @@ if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
 
 
+
+
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import app, db
 
 def init_db():
     with app.app_context():
-        db.create_all()
+        pass
 
 if __name__ == '__main__':
     init_db()
